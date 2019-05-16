8c28067d6a6f4a50efe8392bd696b2dc6983fc05
veeravelan <viruveera@gmail.com>
2019-05-16 16:43:53 +0530

    'Initial'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FundComponent } from './fund/fund.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FundComponent
   ],
   imports: [
     BrowserModule,
