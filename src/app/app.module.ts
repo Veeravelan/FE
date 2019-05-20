37dfa46566d9a51a1223adee3b398486b9169248
veeravelan <viruveera@gmail.com>
2019-05-20 20:12:06 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FundComponent } from './fund/fund.component';
+
+// Import angular-fusioncharts
+import { FusionChartsModule } from 'angular-fusioncharts';
+
+// Import FusionCharts library and chart modules
+import * as FusionCharts from 'fusioncharts';
+import * as Charts from 'fusioncharts/fusioncharts.charts';
+
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+// Pass the fusioncharts library and chart modules
+FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme);
 
 @NgModule({
   declarations: [
@@ -12,7 +27,10 @@ import { FundComponent } from './fund/fund.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    FusionChartsModule,
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/fund/fund.component.ts
+++ src/app/fund/fund.component.ts
@@ -6,10 +6,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fund.component.scss']
 })
 export class FundComponent implements OnInit {
-
-  constructor() { }
+  public isExpandex = false;
+  dataSource: Object;
+  constructor() {
+      this.dataSource = {
+          'chart': {
+              'caption': 'Countries With Most Oil Reserves [2017-18]',
+              'subCaption': 'In MMbbl = One Million barrels',
+              'xAxisName': 'Country',
+              'yAxisName': 'Reserves (MMbbl)',
+              'numberSuffix': 'K',
+              'theme': 'fusion',
+          },
+          // Chart Data
+          'data': [{
+              'label': 'Venezuela',
+              'value': '290'
+          }, {
+              'label': 'Saudi',
+              'value': '260'
+          }, {
+              'label': 'Canada',
+              'value': '180'
+          }, {
+              'label': 'Iran',
+              'value': '140'
+          }, {
+              'label': 'Russia',
+              'value': '115'
+          }, {
+              'label': 'UAE',
+              'value': '100'
+          }, {
+              'label': 'US',
+              'value': '30'
+          }, {
+              'label': 'China',
+              'value': '30'
+          }]
+      }; // end of this.dataSource
+  } // end of constructor
 
   ngOnInit() {
+  }
+
+  public togglePanel(event) {
+      const getElemHeight = document.getElementById('asset_table_hold').style.height;
+      console.log(getElemHeight);
+      if(parseInt(getElemHeight) > 0) {
+        document.getElementById('asset_table_hold').style.cssText = 'height: 0; overflow: hidden;';
+      } else {
+        document.getElementById('asset_table_hold').style.cssText = 'height: auto; overflow: auto;';
+      }
   }
 
 }
